2b1a1e5d23ceefb21c64d01156f48e53a1d8345a
Eleo Basili <eleonel.basili@gmail.com>
2026-01-24 12:43:03 -0300

    bug: Remove comments for blink app.

--- examples/blink/script.js
+++ examples/blink/script.js
@@ -2,7 +2,4 @@
 
 print("Starting JavaScript execution...");
 
-// Simple GIF display - show_gif_from_sd(filepath, x, y)
-// Screen is 536x240, center is (268, 120)
-// Adjust x,y based on your GIF dimensions: x = (536 - gif_width) / 2, y = (240 - gif_height) / 2
 show_gif_from_sd("/blink.gif", 268, 120);
