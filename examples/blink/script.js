9badc08b3ed53a75b59f9e9f8723bb4a76a77848
Eleo Basili <eleonel.basili@gmail.com>
2026-01-24 12:02:21 -0300

    bug: Center image for blink app.

--- examples/blink/script.js
+++ examples/blink/script.js
@@ -3,4 +3,6 @@
 print("Starting JavaScript execution...");
 
 // Simple GIF display - show_gif_from_sd(filepath, x, y)
-show_gif_from_sd("/blink.gif", 0, 0);
+// Screen is 536x240, center is (268, 120)
+// Adjust x,y based on your GIF dimensions: x = (536 - gif_width) / 2, y = (240 - gif_height) / 2
+show_gif_from_sd("/blink.gif", 268, 120);
